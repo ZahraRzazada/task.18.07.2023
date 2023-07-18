5112c431240391b3d0510a6152f14f9e806e43fa
ZahraRzazada <zahraer@code.edu.az>
2023-07-18 13:42:41 +0400

    task.18.07.2023 was added

--- 18.07.2023/Program.cs
+++ 18.07.2023/Program.cs
@@ -1,3 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+#region TASK1
+//static void Percent()
+//{
+//    Console.WriteLine("Enter percent:");
+//    string numberstring = Console.ReadLine();
+//    int number = Convert.ToInt32(numberstring);
+//    double[] arr = { 10, 15, 20, 100 };
+//    double sum = 0;
+//    for (int i = 0; i < arr.Length; i++)
+//    {
+//        if (arr[i] < 20)
+//        {
+//            arr[i] = arr[i] * number / 100;
+//        }
+//        sum += arr[i];
+//    }
+//    Console.WriteLine(sum);
+//}
+//Percent();
+#endregion
+
+#region TASK2
+static void Letter()
+{
+    Console.WriteLine("Enter number:");
+    string numberstring = Console.ReadLine();
+    int number = Convert.ToInt32(numberstring);
+    string text = Console.ReadLine();
+    for (int i = 0; i < text.Length; i++)
+    {
+        if (i == number)
+        {
+            continue;
+        }
+        Console.WriteLine(text[i]);
+    }
+}
+Letter();
+#endregion
 
